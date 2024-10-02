fd90bad324703777bf8831e9b9bf1b9b8a810c14
Suhel Hasan <suhelhasan202@gmail.com>
2024-10-02 21:45:29 +0530

    new fix

--- src/data/mockData.ts
+++ src/data/mockData.ts
@@ -1,69 +1,25 @@
-// import { Highlight } from "../models/Highlight";
-
-// export const mockHighlights: Highlight[] = [
-//   {
-//     match_id: 1,
-//     sport: "badminton",
-//     video_url: "https://example.com/highlight1.mp4",
-//     key_moments: [
-//       {
-//         time: 30,
-//         description: "Sudeep scores a great point",
-//         start_time: 0,
-//         end_time: 25,
-//       },
-//       {
-//         time: 60,
-//         description: "Saket makes an amazing comeback",
-//         start_time: 70,
-//         end_time: 90,
-//       },
-//     ],
-//     statistics: { calories_burned: 100, shot_accuracy: 90 },
-//   },
-//   {
-//     match_id: 2,
-//     sport: "badminton",
-//     video_url: "https://example.com/highlight2.mp4",
-//     key_moments: [
-//       {
-//         time: 30,
-//         description: "Player A scores a point",
-//         start_time: 0,
-//         end_time: 25,
-//       },
-//       {
-//         time: 60,
-//         description: "Player B makes a great save",
-//         start_time: 70,
-//         end_time: 90,
-//       },
-//     ],
-//     statistics: { calories_burned: 80, shot_accuracy: 85 },
-//   },
-// ];
-
 import { Highlight } from "../models/Highlight";
 
 export const mockHighlights: Highlight[] = [
   {
     match_id: 1,
     sport: "badminton",
-    match_type: "1v1", // New field added
-    user: "Sudeep", // New field added
-    video_url: "https://www.w3schools.com/html/mov_bbb.mp4",
+    match_type: "1v1",
+    user: "Sudeep Naru",
+    video_url:
+      "https://videos.pexels.com/video-files/2932301/2932301-uhd_2732_1440_24fps.mp4",
     key_moments: [
       {
-        time: 30,
-        description: "Sudeep scores a great point",
-        start_time: 0,
-        end_time: 25,
+        time: 3,
+        description: "scores a point",
+        start_time: 3,
+        end_time: 6,
       },
       {
-        time: 60,
-        description: "Saket makes an amazing comeback",
-        start_time: 70,
-        end_time: 90,
+        time: 4,
+        description: "Makes a great save",
+        start_time: 5,
+        end_time: 9,
       },
     ],
     statistics: { calories_burned: 100, shot_accuracy: 90 },
@@ -71,23 +27,45 @@ export const mockHighlights: Highlight[] = [
   {
     match_id: 2,
     sport: "badminton",
-    match_type: "2v2", // New field added
-    user: "PlayerA", // New field added
+    match_type: "2v2",
+    user: "Rahul Guota",
     video_url: "https://www.w3schools.com/html/mov_bbb.mp4",
     key_moments: [
       {
         time: 3,
-        description: "Player A scores a point",
+        description: "Scores a point",
         start_time: 3,
         end_time: 6,
       },
       {
         time: 4,
-        description: "Player B makes a great save",
+        description: "Makes a great save",
         start_time: 5,
         end_time: 9,
       },
     ],
-    statistics: { calories_burned: 80, shot_accuracy: 85 },
+    statistics: { calories_burned: 17, shot_accuracy: 40 },
+  },
+  {
+    match_id: 3,
+    sport: "football",
+    match_type: "1v1",
+    user: "Gungun Negi",
+    video_url: "https://www.w3schools.com/html/mov_bbb.mp4",
+    key_moments: [
+      {
+        time: 3,
+        description: "Scores a point",
+        start_time: 3,
+        end_time: 6,
+      },
+      {
+        time: 4,
+        description: "Makes a great save",
+        start_time: 5,
+        end_time: 9,
+      },
+    ],
+    statistics: { calories_burned: 60, shot_accuracy: 15 },
   },
 ];
